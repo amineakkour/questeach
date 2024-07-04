e52a12412055d77431ec6504b02255e427cd13a1
amine <amineakour6@gmail.com>
2024-07-04 15:16:49 +0100

    Correct for attribute error

--- src/components/config/ActiveTimer.jsx
+++ src/components/config/ActiveTimer.jsx
@@ -17,7 +17,7 @@ export default function ActiveTimer() {
           id="cbx-46" className={styles.inpCbx} 
           checked={timer.isActive} 
           onChange={e => setTimer(timer => {return ({...timer, isActive: e.target.checked})})} />
-        <label for="cbx-46" className={styles.cbx}><span>
+        <label htmlFor="cbx-46" className={styles.cbx}><span>
           <svg viewBox="0 0 12 10" height="10px" width="12px"><polyline points="1.5 6 4.5 9 10.5 1"></polyline></svg>
           </span><span></span>
         </label>
