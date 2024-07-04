eff8c76e4ac613bd95d0804cc97cb59e91bec930
amine <amineakour6@gmail.com>
2024-07-04 15:21:28 +0100

    Add continuo anyway button

--- src/pages/DeviceNotAllowed.jsx
+++ src/pages/DeviceNotAllowed.jsx
@@ -1,9 +1,10 @@
-export default function DevilceNotAllowed() {
+export default function DevilceNotAllowed({setContinuo}) {
     return (
         <div style={{padding: "10px"}}>
             <h2>Error: 403</h2>
             <h3>Small Screens not allowed</h3>
             <p>Please access the website using a device with a screen width greater than 600px.</p>
+            <button onClick={() => setContinuo(true)}>Continuo Anyway?</button>
         </div>
     )
 }
