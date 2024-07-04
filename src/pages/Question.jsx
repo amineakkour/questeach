ba2494fe31363840a47928bfa6c8780236bfed42
amine <amineakour6@gmail.com>
2024-07-04 15:28:00 +0100

    Add deactivate question and timer

--- src/pages/Question.jsx
+++ src/pages/Question.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import styles from "../styles/question.module.css"
 import { useStarterContext } from '../context/StarterProvider'
 import { useNavigate, useParams } from 'react-router';
 
 export default function Question() {
-  const {file, setFile, activePlayer} = useStarterContext();
+  const {file: {fcontent}, file, setFile, setActivePlayer, players, timer} = useStarterContext();
   const {id} = useParams();
-  const question = file.fcontent[id -1];
+  const question = fcontent[id -1];
   const answers = question.answers
   const navigate = useNavigate();
   const [answerId, setAnswerId] = useState(null);
-
+  const [seconds, setSeconds] = useState(parseInt(timer.seconds) || 0);
 
   function correctAnswers(){
     var correctAnswers = []
@@ -30,7 +30,7 @@ export default function Question() {
 
     return answer
   }
-  
+
   function checkAnswer(e){
     e.preventDefault()
     console.log(getAnswer(answerId))
@@ -40,19 +40,48 @@ export default function Question() {
     // console.log(correctAnswers())
   }
 
-  function cancelQuestion() {
-    console.log("cancel")
+  function deActivateQuestion() {
+    const new_fcontent = [...fcontent.filter(q => q.id != id), {...question, isDeactivated: true}];
+    setFile(c => {return {...c, fcontent: new_fcontent}});
   }
+
+  function moveToNextPlayer() {
+    const maxActivate = players.length -1;
+    setActivePlayer(ap =>  parseInt(ap) +1 > maxActivate ? 0 : parseInt(ap) +1);
+  }
+
+  function cancelQuestion() {
+    moveToNextPlayer()
+    deActivateQuestion()
+    navigate('/game')
+  }
+
+  useEffect(() => {
+  if (timer.seconds) {
+    var intSeconds = seconds
+    const myInterval = setInterval(() => {
+      intSeconds -= 1;
+      setSeconds(s => intSeconds);
+
+      if(intSeconds <= 0) {
+        clearInterval(myInterval);
+        cancelQuestion();
+      }
+    }, 1000);
+
+    return () => clearInterval(myInterval);
+  }
+}, [timer.seconds]);
 
   return (
     <form className={styles.container} onSubmit={e => checkAnswer(e)}>
       <div className={styles.questionWrapper}>
         <div className={styles.header}>
           <h3 className={styles.title}>{question.text}</h3>
-          <div className={styles.timer}>
+          {timer.isActive && <div className={styles.timer}>
             <i className="fa-regular fa-clock"></i>
-            <span className='timer'>00:20</span>
-          </div>
+            <span className='timer'>00:{seconds}</span>
+          </div>}
         </div>
 
         <div className={styles.answers}>
